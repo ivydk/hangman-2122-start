f299c37a4a2c0f5ddaf15476cb14f368d273582e
ivydk <dekk0123@hz.nl>
2021-11-15 15:57:04 +0100

    opdrachten 15 okt

--- build/app.js
+++ build/app.js
@@ -1,3 +1,17 @@
+console.log("it's working");
+const arrayWithWords = ['hallo', 'appel', 'okeej'];
+const word = randomWord(arrayWithWords).split('');
+let attempts = 0;
+let guessedCharactersInWord;
+let dashes;
+let guessedLettersInWord;
+function init() {
+    writeAlphabetToTheDom();
+    addDashes();
+    addGuessedLetterToDOM(4);
+    addAttempsToTheDOM(attempts);
+    console.log(word);
+}
 function writeAlphabetToTheDom() {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     const keyboard = document.querySelector('#keyboard');
@@ -12,8 +26,24 @@ function writeAlphabetToTheDom() {
 function randomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
-function init() {
-    writeAlphabetToTheDom();
-}
 window.addEventListener('load', init);
+function randomWord(array) {
+    const randomWord = array[Math.floor(Math.random() * array.length)];
+    return randomWord;
+}
+function addDashes() {
+    const length = word.length;
+    const listWithLetters = document.querySelector('#letters');
+    for (let i = 0; i < length; i += 1) {
+        const listItem = document.createElement('li');
+        listItem.setAttribute('id', `char${i}`);
+        listWithLetters.appendChild(listItem).innerHTML = '_';
+    }
+}
+function addGuessedLetterToDOM(charIndex) {
+    document.querySelector(`#char${charIndex}`).innerHTML = word[charIndex];
+}
+function addAttempsToTheDOM(numberOfAttempts) {
+    document.querySelector('#attempt').innerHTML = numberOfAttempts.toString();
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,38 @@
+console.log("it's working");
+
 // Global variables
+
+const arrayWithWords: string[] = ['hallo', 'appel', 'okeej'];
+
+const word: string[] = randomWord(arrayWithWords).split('');
+
+let attempts = 0;
+
+let guessedCharactersInWord: number;
+
+let dashes: string;
+
+let guessedLettersInWord: string;
+
+/**
+ * Function to initialize the programme
+ */
+function init() {
+  // write the alphabet keyboard to the DOM
+  writeAlphabetToTheDom();
+
+  // write the dashes to the DOM
+  addDashes();
+
+  // chanhes a guessed letter in the DOM
+  addGuessedLetterToDOM(4);
+
+  // write the attempts to the DOM
+  addAttempsToTheDOM(attempts);
+
+  console.log(word)
+}
+
 
 /**
  * Function to write the alphabet keyboard to the DOM
@@ -16,21 +50,64 @@ function writeAlphabetToTheDom() {
 }
 
 /**
- * Returns a random number between min and max
- * @param {number} min - lower boundary
- * @param {number} max - upper boundary
- * @returns {number} random number
+ * nn
+ * .
+ * @param min
+ * @param max
+ * @returns
  */
 function randomNumber(min: number, max: number): number {
   return Math.round(Math.random() * (max - min) + min);
 }
 
+window.addEventListener('load', init);
+
 /**
- * Function to initialize the programme
+ * finds a random word in a array with strings
+ * @param array the array with strings
+ * @returns returns a random element from a array
  */
-function init() {
-  // write the alphabet keyboard to the DOM
-  writeAlphabetToTheDom();
+function randomWord(array: string[]): string {
+  const randomWord = array[Math.floor(Math.random() * array.length)];
+  return randomWord
 }
 
-window.addEventListener('load', init);
+/**
+ * deze functie maakt li item aan onder het ul #letters element in de DOM en zet het aantal
+ * dashes op je beeldscherm
+ */
+function addDashes() {
+  // eslint-disable-next-line prefer-destructuring
+  const length = word.length;
+  const listWithLetters = document.querySelector('#letters');
+
+  for (let i = 0; i < length; i += 1) {
+    const listItem: HTMLElement = document.createElement('li');
+    listItem.setAttribute('id', `char${i}`);
+    listWithLetters.appendChild(listItem).innerHTML = '_';
+  }
+}
+
+/**
+ * verandert een dash uit de li items #char... naar een letter
+ *
+ * @param charIndex hoeveelste letter (begint bij 0)
+ * @param guessedLetter welke letter er hier komt te staan
+ */
+function addGuessedLetterToDOM(charIndex: number) {
+  document.querySelector(`#char${charIndex}`).innerHTML = word[charIndex];
+}
+
+/**
+ * zet het aantal pogingen in de DOM
+ *
+ * @param numberOfAttempts het aantal keer dat er al geraden is
+ */
+function addAttempsToTheDOM(numberOfAttempts: number) {
+  document.querySelector('#attempt').innerHTML = numberOfAttempts.toString();
+}
+
+// function addClickEvent() {
+
+// }
+
