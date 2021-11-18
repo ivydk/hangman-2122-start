20d89bdf034b326d93c5ddc65d7f422eee6a9e9c
ivydk <dekk0123@hz.nl>
2021-11-18 10:42:55 +0100

    before class

--- build/app.js
+++ build/app.js
@@ -1,51 +1,35 @@
 console.log("it's working");
 const arrayWithWords = ['hallo', 'appel', 'okeej', 'aandacht', 'welkom', 'scooters'];
+const guessedCharactersInWord = [];
+let attempts = 6;
+function randomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+function randomWord(array) {
+    const randomWordFromArray = arrayWithWords[randomNumber(0, array.length - 1)];
+    return randomWordFromArray;
+}
 const word = randomWord(arrayWithWords);
 const charsInWord = word.split('', word.length);
-const guessedCharactersInWord = [];
 charsInWord.forEach(() => {
     guessedCharactersInWord.push('_');
 });
 console.log(guessedCharactersInWord);
-let attempts = 6;
-function init() {
-    writeAlphabetToTheDom();
-    addDashes();
-    addAttempsToTheDOM(attempts);
-    console.log(word);
-}
-function writeAlphabetToTheDom() {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    const keyboard = document.querySelector('#keyboard');
-    alphabet.forEach((element) => {
-        const divKey = document.createElement('div');
-        divKey.id = element;
-        divKey.classList.add('key');
-        divKey.innerHTML = element;
-        keyboard.append(divKey);
-        divKey.addEventListener('click', () => {
-            console.log(element);
-            if (inArray(element, charsInWord)) {
-                console.log('yess');
-                charOnWichIndex(charsInWord, element);
-            }
-            else {
-                attempts -= 1;
-                addAttempsToTheDOM(attempts);
-            }
-        });
-    });
-}
-function charOnWichIndex(array, char) {
+function checkIfArrayIsEqual(array1, array2) {
     let index = 0;
-    array.forEach(() => {
-        if (array[index] === char) {
-            guessedCharactersInWord[index] = char;
-            console.log(guessedCharactersInWord);
-            addGuessedLetterToDOM(index);
-        }
-        index += 1;
-    });
+    let counter = 0;
+    if (array1.length === array2.length) {
+        array1.forEach(() => {
+            if (array1[index] === array2[index]) {
+                counter += 1;
+            }
+            index += 1;
+        });
+    }
+    if (counter === array1.length) {
+        return true;
+    }
+    return false;
 }
 function inArray(char, array) {
     const count = array.length;
@@ -55,13 +39,6 @@ function inArray(char, array) {
         }
     }
     return false;
-}
-function randomNumber(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-}
-function randomWord(array) {
-    const randomWordFromArray = arrayWithWords[randomNumber(0, array.length - 1)];
-    return randomWordFromArray;
 }
 function addDashes() {
     const length = charsInWord.length;
@@ -77,6 +54,58 @@ function addGuessedLetterToDOM(charIndex) {
 }
 function addAttempsToTheDOM(numberOfAttempts) {
     document.querySelector('#attempt').innerHTML = numberOfAttempts.toString();
+}
+function charOnWichIndex(array, char) {
+    let index = 0;
+    array.forEach(() => {
+        if (array[index] === char) {
+            guessedCharactersInWord[index] = char;
+            console.log(guessedCharactersInWord);
+            addGuessedLetterToDOM(index);
+        }
+        index += 1;
+    });
+}
+function whenEvent(element) {
+    const target = element.target;
+    const e = target.getAttribute('id');
+    if (inArray(e, charsInWord)) {
+        charOnWichIndex(charsInWord, e);
+    }
+    else {
+        attempts -= 1;
+        addAttempsToTheDOM(attempts);
+    }
+    const listItem = document.querySelector(`#${e}`);
+    listItem.classList.add('idle');
+    const wordInDOM = document.querySelector('#letters');
+    const keyboardInDOM = document.querySelector('#keyboard');
+    if (checkIfArrayIsEqual(guessedCharactersInWord, charsInWord)) {
+        wordInDOM.classList.add('winner');
+        keyboardInDOM.classList.add('idle');
+    }
+    if (attempts <= 0) {
+        wordInDOM.classList.add('lost');
+        keyboardInDOM.classList.add('idle');
+    }
+}
+function writeAlphabetToTheDom() {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const keyboard = document.querySelector('#keyboard');
+    alphabet.forEach((element) => {
+        const divKey = document.createElement('div');
+        divKey.id = element;
+        divKey.classList.add('key');
+        divKey.innerHTML = element;
+        keyboard.append(divKey);
+        divKey.addEventListener('click', whenEvent);
+    });
+}
+function init() {
+    writeAlphabetToTheDom();
+    addDashes();
+    addAttempsToTheDOM(attempts);
+    console.log(word);
 }
 window.addEventListener('load', init);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,62 +2,111 @@ console.log("it's working");
 
 // Global variables
 const arrayWithWords: string[] = ['hallo', 'appel', 'okeej', 'aandacht', 'welkom', 'scooters'];
+const guessedCharactersInWord: string[] = [];
+let attempts = 6;
+
+/**
+ * vind een random nummer
+ *
+ * @param min minimun of the random nummer
+ * @param max maximin of the random nummer
+ * @returns random nummer
+ */
+function randomNumber(min: number, max: number): number {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+/**
+ * finds a random word in a array with strings
+ *
+ * @param array the array with strings
+ * @returns returns a random element from a array
+ */
+function randomWord(array: string[]): string {
+  const randomWordFromArray = arrayWithWords[randomNumber(0, array.length - 1)];
+  return randomWordFromArray;
+}
+
 const word: string = randomWord(arrayWithWords);
 const charsInWord: string[] = word.split('', word.length);
-const guessedCharactersInWord: string[] = [];
 charsInWord.forEach(() => {
   guessedCharactersInWord.push('_');
 });
 console.log(guessedCharactersInWord);
 
-let attempts = 6;
-
 /**
- * Function to initialize the programme
+ * checks if the arrays are equal
+ *
+ * @param array1 first array you want to check
+ * @param array2 second array you want to check
+ * @returns h
  */
-function init() {
-  // write the alphabet keyboard to the DOM
-  writeAlphabetToTheDom();
-  // write the dashes to the DOM
-  addDashes();
-  // write total of attempts in the DOM
-  addAttempsToTheDOM(attempts);
-  console.log(word);
+function checkIfArrayIsEqual(array1: string[], array2: string[]): boolean {
+  let index = 0;
+  let counter = 0;
+  if (array1.length === array2.length) {
+    array1.forEach(() => {
+      if (array1[index] === array2[index]) {
+        counter += 1;
+      }
+      index += 1;
+    });
+  }
+  if (counter === array1.length) {
+    return true;
+  }
+  return false;
 }
 
 /**
- * Function to write the alphabet keyboard to the DOM
+ * checkt of een bepaalde char in de string zit.
+ *
+ * @param char de char die je wilt cheken
+ * @param array je wilt weten of de char in deze array zit
+ * @returns boolean die aangeeft of de char in de array zit of niet
  */
-function writeAlphabetToTheDom() {
-  // let currenkey: string;
-  const alphabet: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  const keyboard: HTMLDivElement = document.querySelector('#keyboard');
-  alphabet.forEach((element) => {
-    const divKey: HTMLDivElement = document.createElement('div');
-    divKey.id = element;
-    divKey.classList.add('key');
-    divKey.innerHTML = element;
-    keyboard.append(divKey);
+function inArray(char: string, array: string[]) {
+  const count = array.length;
+  for (let i = 0; i < count; i += 1) {
+    if (array[i] === char) {
+      return true;
+    }
+  }
+  return false;
+}
 
-    divKey.addEventListener('click', () => {
-      // voeg toe aan guessedLetterToDOM, guessLetter functie aanmaken?
-      console.log(element);
+/**
+ * deze functie maakt li item aan onder het ul #letters element in de DOM en zet het aantal
+ * dashes op je beeldscherm
+ */
+function addDashes() {
+  // eslint-disable-next-line prefer-destructuring
+  const length = charsInWord.length;
+  const listWithLetters = document.querySelector('#letters');
 
-      // maak hier even een functie van :)
-      // is char in the word, on witch index
+  for (let i = 0; i < length; i += 1) {
+    const listItem: HTMLElement = document.createElement('li');
+    listItem.setAttribute('id', `char${i}`);
+    listWithLetters.appendChild(listItem).innerHTML = '_';
+  }
+}
 
-      if (inArray(element, charsInWord)) {
-        // add char on right index
-        console.log('yess');
-        charOnWichIndex(charsInWord, element);
-        // add this letter to the dom
-      } else {
-        // als het de verkeerde letter is
-        attempts -= 1;
-        addAttempsToTheDOM(attempts);
-      }
-    });
-  });
+/**
+ * changes a dach to a li item #char.. to a char
+ *
+ * @param charIndex hoeveelste letter (begint bij 0)
+ */
+function addGuessedLetterToDOM(charIndex: number) {
+  document.querySelector(`#char${charIndex}`).innerHTML = charsInWord[charIndex];
+}
+
+/**
+ * puts the attempts in the DOM
+ *
+ * @param numberOfAttempts het aantal keer dat er al geraden is
+ */
+function addAttempsToTheDOM(numberOfAttempts: number) {
+  document.querySelector('#attempt').innerHTML = numberOfAttempts.toString();
 }
 
 /**
@@ -79,82 +128,66 @@ function charOnWichIndex(array: string[], char: string) {
 }
 
 /**
- * checkt of een bepaalde char in de string zit.
  *
- * @param char de char die je wilt cheken
- * @param array je wilt weten of de char in deze array zit
- * @returns boolean die aangeeft of de char in de array zit of niet
+ * @param element the char
  */
-function inArray(char: string, array: string[]) {
-  const count = array.length;
-  for (let i = 0; i < count; i += 1) {
-    if (array[i] === char) {
-      return true;
-    }
+function whenEvent(element: Event) {
+  const target: HTMLElement = element.target as HTMLElement;
+  const e = target.getAttribute('id');
+  if (inArray(e, charsInWord)) {
+    // add char on right index
+    charOnWichIndex(charsInWord, e);
+    // add this letter to the dom
+  } else {
+    // als het de verkeerde letter is
+    attempts -= 1;
+    addAttempsToTheDOM(attempts);
   }
-  return false;
-}
+  // class .idle maken
+  const listItem: HTMLElement = document.querySelector(`#${e}`);
+  listItem.classList.add('idle');
 
-/**
- * vind een random nummer
- *
- * @param min
- * @param max
- * @returns
- */
-function randomNumber(min: number, max: number): number {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
-/**
- * finds a random word in a array with strings
- * @param array the array with strings
- * @returns returns a random element from a array
- */
-function randomWord(array: string[]): string {
-  const randomWordFromArray = arrayWithWords[randomNumber(0, array.length - 1)];
-  return randomWordFromArray;
-}
-
-/**
- * deze functie maakt li item aan onder het ul #letters element in de DOM en zet het aantal
- * dashes op je beeldscherm
- */
-function addDashes() {
-  // eslint-disable-next-line prefer-destructuring
-  const length = charsInWord.length;
-  const listWithLetters = document.querySelector('#letters');
-
-  for (let i = 0; i < length; i += 1) {
-    const listItem: HTMLElement = document.createElement('li');
-    listItem.setAttribute('id', `char${i}`);
-    listWithLetters.appendChild(listItem).innerHTML = '_';
-    // guessedChars.push('_');
+  const wordInDOM: HTMLElement = document.querySelector('#letters');
+  const keyboardInDOM: HTMLElement = document.querySelector('#keyboard');
+  if (checkIfArrayIsEqual(guessedCharactersInWord, charsInWord)) {
+    wordInDOM.classList.add('winner');
+    keyboardInDOM.classList.add('idle');
+  }
+  if (attempts <= 0) {
+    wordInDOM.classList.add('lost');
+    keyboardInDOM.classList.add('idle');
   }
 }
 
 /**
- * verandert een dash uit de li items #char... naar een letter
- *
- * @param charIndex hoeveelste letter (begint bij 0)
- * @param guessedLetter welke letter er hier komt te staan
+ * Function to write the alphabet keyboard to the DOM
  */
-function addGuessedLetterToDOM(charIndex: number) {
-  document.querySelector(`#char${charIndex}`).innerHTML = charsInWord[charIndex];
-  // guessedChars[charIndex] = charsInWord[charIndex];
+function writeAlphabetToTheDom() {
+  // let currenkey: string;
+  const alphabet: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const keyboard: HTMLDivElement = document.querySelector('#keyboard');
+  alphabet.forEach((element) => {
+    const divKey: HTMLDivElement = document.createElement('div');
+    divKey.id = element;
+    divKey.classList.add('key');
+    divKey.innerHTML = element;
+    keyboard.append(divKey);
+
+    divKey.addEventListener('click', whenEvent);
+  });
 }
 
 /**
- * zet het aantal pogingen in de DOM
- *
- * @param numberOfAttempts het aantal keer dat er al geraden is
+ * Function to initialize the programme
  */
-function addAttempsToTheDOM(numberOfAttempts: number) {
-  document.querySelector('#attempt').innerHTML = numberOfAttempts.toString();
+function init() {
+  // write the alphabet keyboard to the DOM
+  writeAlphabetToTheDom();
+  // write the dashes to the DOM
+  addDashes();
+  // write total of attempts in the DOM
+  addAttempsToTheDOM(attempts);
+  console.log(word);
 }
-
-// function addClickEvent() {
-
-// }
 
 window.addEventListener('load', init);
